7260cb673063584218ff05f34da440b2378f8d4f
HanikLZ <haniklz@gmail.com>
2025-07-04 01:27:18 +0800

    fat jar configuration

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,45 +1,72 @@
+import java.util.Properties
+import kotlin.apply
+
 plugins {
     `kotlin-dsl`
     alias(libs.plugins.gradle.publish)
 }
 
-group = "org.mdvsc"
+group = "cc.mdvsc"
 version = "1.0.0"
 
 tasks.withType<JavaCompile> {
     options.compilerArgs.add("-Xlint:-unchecked")
 }
 
+val embeddedJars: Configuration by configurations.creating
+configurations.compileOnly.get().extendsFrom(embeddedJars)
+
 dependencies {
     implementation(libs.asm)
     implementation(libs.asm.commons)
     implementation(libs.proguard.gradle)
     implementation(libs.kotlin.gradle.plugin)
-
-    implementation(files("libs/rapc.jar"))
-    implementation(files("libs/jadtool.jar"))
+    embeddedJars(files("libs/rapc.jar"))
+    embeddedJars(files("libs/jadtool.jar"))
     compileOnly(files("libs/net_rim_api.jar"))
 }
 
+
+val pluginName = "rapcBuilder"
+
 gradlePlugin {
+    website = "https://github.com/HanikLZ/bb-rpac"
+    vcsUrl = "https://github.com/HanikLZ/bb-rpac"
     plugins {
-        register("") {
+        create(pluginName) {
             id = "${project.group}.${project.name}"
-            implementationClass = "$id.RapcPlugin"
+            isAutomatedPublishing = true
+            displayName = "RapcBuilder Plugin"
+            description = "A Gradle plugin that brings modern Kotlin development to classic BlackBerry and J2ME devices."
+            tags = listOf("kotlin", "java", "javame", "j2me", "blackberry")
+            implementationClass = "org.mdvsc.rapc.RapcPlugin"
         }
     }
 }
-/*
 
-pluginBundle {
-    website = 'https://github.com/your-username/my-awesome-plugin' // 你的插件网站地址
-    vcsUrl = 'https://github.com/your-username/my-awesome-plugin.git' // 你的版本控制系统地址
-    description = 'A brief description of my awesome plugin.' // 插件描述
-    tags = ['example', 'awesome', 'plugin'] // 标签，方便搜索
+tasks.jar {
+    from(embeddedJars.map(::zipTree))
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
 
-    plugins {
-        myAwesomePlugin {
-            displayName = 'My Awesome Gradle Plugin' // 在门户网站上显示的插件名称
+tasks.withType<Jar>().configureEach {
+    if (name == "sourcesJar") include("**/*.java", "**/*.kt")
+}
+
+val mavenUrl: String by project
+val localProperties = Properties().apply {
+    val localPropertiesFile = rootProject.file("local.properties")
+    if (localPropertiesFile.exists()) localPropertiesFile.reader().use { load(it) }
+}
+
+publishing {
+    repositories {
+        maven {
+            credentials {
+                url = uri(mavenUrl)
+                username = localProperties.getProperty("publish.user")
+                password = localProperties.getProperty("publish.password")
+            }
         }
     }
-}*/
+}
